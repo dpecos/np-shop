5728f21fcd1e077d351f864c4ce2f51a369ed079
dpecos <dpecos@a050b9d1-0941-0410-b075-8f679339ff21>
2007-12-17 17:12:53 +0000

    Print shipping labels capability

--- trunk/admin/orderDetail.php
+++ trunk/admin/orderDetail.php
@@ -1,4 +1,4 @@
-<? 
+<?php
 define('APP_ROOT', "../");
 require_once(APP_ROOT."/config/main.php");
 require_once(APP_ROOT."/common/commonFunctions.php");
@@ -15,40 +15,42 @@ $user->_dbLoad($order->user->id);
 <html>
     <head>
         <style>
-            <? include_once(APP_ROOT.'/admin/style.css'); ?>
+            <?php include_once(APP_ROOT.'/admin/style.css'); ?>
         </style>
     </head>
     <body>
-        <h1>Detalle del pedido <?= formatOrderId($order) ?></h1>
+        <h1>Detalle del pedido <?php echo formatOrderId($order) ?></h1>
 
         <h2>Resumen del pedido</h2>
         <center>
         <table boder="0">
-            <tr><td>Fecha:</td><td><?= date("d/m/Y H:i:s", $order->date) ?></td></tr>
-            <tr><td>Estado:</td><td><?= $order->orderStatus ?></td></tr>
-            <tr><td>Importe Total:</td><td><?= $order->getTotal(1) ?> &euro;</td></tr>
+            <tr><td>Fecha:</td><td><?php echo date("d/m/Y H:i:s", $order->date) ?></td></tr>
+            <tr><td>Estado:</td><td><?php echo $order->orderStatus ?></td></tr>
+            <tr><td>Importe Total:</td><td><?php echo $order->getTotal(1) ?> &euro;</td></tr>
             <tr>
                 <td>Datos de facturación:</td>
                 <td>
-                    <?= $order->user->billingData['name'] ?> <?= $order->user->billingData['surname'] ?><br>
-                    <?= $order->user->billingData['address'] ?> <?= $order->user->billingData['address2'] ?><br>
-                    Telf. <?= $order->user->billingData['phone'] ?><br>
-                    CP: <?= $order->user->billingData['postalCode'] ?> <?= $order->user->billingData['city'] ?><br>
-                    <?= getProvinceName($order->user->billingData['province']) ?> <?= getCountryName($order->user->billingData['country']) ?> 
+                    <?php echo $order->user->billingData['name'] ?> <?php echo $order->user->billingData['surname'] ?><br>
+                    <?php echo $order->user->billingData['address'] ?> <?php echo $order->user->billingData['address2'] ?><br>
+                    Telf. <?php echo $order->user->billingData['phone'] ?><br>
+                    CP: <?php echo $order->user->billingData['postalCode'] ?> <?php echo $order->user->billingData['city'] ?><br>
+                    <?php echo getProvinceName($order->user->billingData['province']) ?> (<?php echo getCountryName($order->user->billingData['country']) ?>) 
                 </td> 
             </tr>
             <tr>
                 <td>Datos de envío:</td>
                 <td>
-                    <?= $order->user->shippingData['name'] ?> <?= $order->user->shippingData['surname'] ?><br>
-                    <?= $order->user->shippingData['address'] ?> <?= $order->user->shippingData['address2'] ?><br>
-                    Telf. <?= $order->user->shippingData['phone'] ?><br>
-                    CP: <?= $order->user->shippingData['postalCode'] ?> <?= $order->user->shippingData['city'] ?><br>
-                    <?= getProvinceName($order->user->shippingData['province']) ?> <?= getCountryName($order->user->shippingData['country']) ?> 
+                    <?php echo $order->user->shippingData['name'] ?> <?php echo $order->user->shippingData['surname'] ?><br>
+                    <?php echo $order->user->shippingData['address'] ?> <?php echo $order->user->shippingData['address2'] ?><br>
+                    Telf. <?php echo $order->user->shippingData['phone'] ?><br>
+                    CP: <?php echo $order->user->shippingData['postalCode'] ?> <?php echo $order->user->shippingData['city'] ?><br>
+                    <?php echo getProvinceName($order->user->shippingData['province']) ?> (<?php echo getCountryName($order->user->shippingData['country']) ?>) 
                 </td>
             </tr>            
-            <tr><td>Email del comprador:</td><td><?= $user->email ?></td></tr>
+            <tr><td>Email del comprador:</td><td><?php echo $user->email ?></td></tr>
         </table>
+        <br/>
+        <a href="#" onclick="javascript:window.open('shippingLabel.php?orderId=<?php echo $order->orderId ?>', 'Etiqueta', 'width=340,height=280,status=no,toolbar=no,location=no,scrollbars=no,resizble=no'); return false;">Generar etiqueta de envío</a>
         </center>
         <br/>
     
@@ -63,42 +65,42 @@ $user->_dbLoad($order->user->id);
                 <th>Cantidad</th>
                 <th>Total</th>
             </tr>
-            <? foreach ($order->items as $item) { ?>
+            <?php foreach ($order->items as $item) { ?>
             <tr>
                 <td align="center"><img src="../images/<?php echo $item->id ?>_p.jpg"/></td>
-                <td align="center"><a href="../flows/showItem.php?itemId=<?php echo $item->id ?>"><?= $item->id ?></a></td>
-                <td><?= $item->name ?></td>
-                <td align="center"><?= $item->prize ?> &euro;</td>
-                <td align="center"><?= $item->quantity ?></td>
-                <td align="center"><?= $item->getSubTotal() ?> &euro;</td>
+                <td align="center"><a href="../flows/showItem.php?itemId=<?php echo $item->id ?>"><?php echo $item->id ?></a></td>
+                <td><?php echo $item->name ?></td>
+                <td align="center"><nobr><?php echo $item->prize ?> &euro;</nobr></td>
+                <td align="center"><?php echo $item->quantity ?></td>
+                <td align="center"><nobr><?php echo $item->getSubTotal() ?> &euro;</nobr></td>
             </tr>
-            <? } ?>
+            <?php } ?>
         </table>
         </center>
         <br/>
                 
-        <h2>Etiquetas de envío</h2>
+        <!--h2>Etiquetas de envío</h2>
         <br/>
         <center>
         <table class="etiquetas">
             <tr>
                 <td>
-                    <?= $order->user->billingData['name'] ?> <?= $order->user->billingData['surname'] ?><br>
-                    <?= $order->user->billingData['address'] ?> <?= $order->user->billingData['address2'] ?><br>
-                    Telf. <?= $order->user->billingData['phone'] ?><br>
-                    CP: <?= $order->user->billingData['postalCode'] ?> <?= $order->user->billingData['city'] ?><br>
-                    <?= getProvinceName($order->user->billingData['province']) ?> <?= getCountryName($order->user->billingData['country']) ?> 
+                    <?php echo $order->user->billingData['name'] ?> <?php echo $order->user->billingData['surname'] ?><br>
+                    <?php echo $order->user->billingData['address'] ?> <?php echo $order->user->billingData['address2'] ?><br>
+                    Telf. <?php echo $order->user->billingData['phone'] ?><br>
+                    CP: <?php echo $order->user->billingData['postalCode'] ?> <?php echo $order->user->billingData['city'] ?><br>
+                    <?php echo getProvinceName($order->user->billingData['province']) ?> <?php echo getCountryName($order->user->billingData['country']) ?> 
                 </td> 
                 <td>
-                    <?= $order->user->shippingData['name'] ?> <?= $order->user->shippingData['surname'] ?><br>
-                    <?= $order->user->shippingData['address'] ?> <?= $order->user->shippingData['address2'] ?><br>
-                    Telf. <?= $order->user->shippingData['phone'] ?><br>
-                    CP: <?= $order->user->shippingData['postalCode'] ?> <?= $order->user->shippingData['city'] ?><br>
-                    <?= getProvinceName($order->user->shippingData['province']) ?> <?= getCountryName($order->user->shippingData['country']) ?> 
+                    <?php echo $order->user->shippingData['name'] ?> <?php echo $order->user->shippingData['surname'] ?><br>
+                    <?php echo $order->user->shippingData['address'] ?> <?php echo $order->user->shippingData['address2'] ?><br>
+                    Telf. <?php echo $order->user->shippingData['phone'] ?><br>
+                    CP: <?php echo $order->user->shippingData['postalCode'] ?> <?php echo $order->user->shippingData['city'] ?><br>
+                    <?php echo getProvinceName($order->user->shippingData['province']) ?> <?php echo getCountryName($order->user->shippingData['country']) ?> 
                 </td>
             </tr>
         </table>
         </center>
-        <br/>
+        <br/-->
     </body>
 </html>
