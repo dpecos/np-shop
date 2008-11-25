9b967e08302be8d439d00a05a501333a0aecd698
dpecos <dpecos@a050b9d1-0941-0410-b075-8f679339ff21>
2008-11-25 14:24:04 +0000

    Fix to prevent navigator cache

--- trunk/common/commonFunctions.php
+++ trunk/common/commonFunctions.php
@@ -16,7 +16,13 @@ require_once(APP_ROOT."/lib/NPLib_Object.php");
 
 function showSkin($file, $id = null) {
 	global $skinFiles, $npshop;
-	
+	 
+	 header("Expires: Sat, 01 Jan 2000 01:00:00 GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    
     loadSkinDescriptor();
 	
     $skinFile = null;
